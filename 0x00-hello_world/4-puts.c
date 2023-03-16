eeff5b6dbfa4e60a1f7d9b33c37ca3c60ea31235
Omar Khaled <o.khaled6789@gmail.com>
2023-03-16 16:21:43 -0700

    edited 4-puts using betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void){
-	char str[]="Programming is like building a multilingual puzzle";
+/**
+ * main - Entry point, use the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[]="\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
 }
