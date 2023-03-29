e0ce23a7d4652e68b3b7841c5be512f647e8f682
Omar Khaled <o.khaled6789@gmail.com>
2023-03-29 11:26:33 -0700

    added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (s1[i] - s2[i]);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 
 0-strcat.c: a function that concatenates two strings.    
 1-strncat.c: a function that concatenates two strings with at most n bytes from src  
-2-strncpy.c:  a function that copies a string.
+2-strncpy.c:  a function that copies a string.  
+3-strcmp.c:  a function that compares two strings.
