5a0aa1e2c55364ca5ea8e42e48b1ee4e2fb2d107
Omar Khaled <o.khaled6789@gmail.com>
2023-04-08 09:07:15 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments passed through the command line
+ * @argv: an array of strings contains the arguments passed to the command line
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
