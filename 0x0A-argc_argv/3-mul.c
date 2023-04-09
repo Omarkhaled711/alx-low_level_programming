6e39e201edccaa74941b12e642e0f0fb873b8c7a
Omar Khaled <o.khaled6789@gmail.com>
2023-04-09 01:53:56 -0700

    edited betty style in 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int prod;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	prod = atoi(argv[1]) * atoi( argv[2]);
+	prod = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", prod);
 	return (0);
 }
