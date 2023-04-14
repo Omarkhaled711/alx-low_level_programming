3a2a3326fb74984441f1a79114f89f085d07ddd2
Omar Khaled <o.khaled6789@gmail.com>
2023-04-13 18:06:14 -0700

    edited betty style in 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,7 +5,7 @@
  * _realloc - similar to the realloc funtionin the stldib.h
  * @ptr: pointer to the existing memory
  * @old_size: old size
- * @new_size: new size 
+ * @new_size: new size
  * Return: a void pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
-	{	free (ptr);
+	{	free(ptr);
 		return (NULL);
 	}
 	p = malloc(new_size);
@@ -29,7 +29,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		old_size = new_size;
 	for (i = 0; i < old_size && ptr != NULL; i++)
 		access1[i] = access2[i];
-
+	if (p != ptr)
+		free(ptr);
 	return (p);
 
 }
