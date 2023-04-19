e44fbb698381906a9400fdf108207173e6815654
Omar Khaled <o.khaled6789@gmail.com>
2023-04-19 16:47:54 +0200

    added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints the string given in a custom chosen way
+ * @name: the string to print
+ * @f: the function pointer that specifies the format
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+#ALX task: 0x0F. C - Function pointers  
+* 0-print_name.c: a function that prints a string according to a function_pointer specifications  
+
