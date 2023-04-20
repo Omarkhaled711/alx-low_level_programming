6ac866bcaca9c7b502137bb33706061525ffbc79
Omar Khaled <o.khaled6789@gmail.com>
2023-04-21 01:27:08 +0200

    handled a corner case in 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,7 +15,7 @@ void print_all(const char * const format, ...)
 	va_list formatPtr;
 
 	va_start(formatPtr, format);
-	while (format[i] != 0)
+	while (format != NULL && format[i] != 0)
 	{
 		switch (format[i])
 		{
