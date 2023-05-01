a4350928c3277a32df478050cbe866e528ec4195
Omar Khaled <o.khaled6789@gmail.com>
2023-05-02 00:10:49 +0300

    handled a corner case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
+	else if (*head == NULL)
+		return (NULL);
 	cur = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
