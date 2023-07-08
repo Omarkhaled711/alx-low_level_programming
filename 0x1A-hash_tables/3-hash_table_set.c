650ac450c1369f119f6b5bad5c0ef2b25cbd1e86
Omar Khaled <o.khaled6789@gmail.com>
2023-07-08 10:29:55 +0300

    handled a segmentation fault error corner case

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	prev = current;
 	while (current != NULL)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (current->key != NULL && strcmp(current->key, key) == 0)
 		{
 			free(current->value);
 			current->value = strdup(value);
