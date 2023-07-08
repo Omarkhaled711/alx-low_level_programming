679582fafebea17f9e3a27de24a1f3ba9c38fd83
Omar Khaled <o.khaled6789@gmail.com>
2023-07-08 05:20:22 +0300

    added 1-djb2.c

--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,3 +1,4 @@
 # Alx task: Hash table
 
 * 0-hash_table_create.c: create the hash_table function
+* 1-djb2.c: a hash function implementing the djb2 algorithm.
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_T
+#define HASH_T
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -30,3 +33,5 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+#endif
