c03562ad3f22336606f27eb21bff16dfd9e7a4fb
Omar Khaled <o.khaled6789@gmail.com>
2023-07-08 09:13:56 +0300

    handled some corner cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current, *prev, *new;
 
-	if (strcmp(key, "") == 0)
+	if (ht == NULL || ht->array == NULL)
+		return (0);
+	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
